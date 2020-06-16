929212329a2ce02937831ed5bcade711c42bb2aa
kunxin-chor <chorkunxin@yahoo.com>
2020-06-16 14:13:44 +0000

    Added TTT example

---
+++ ttt/src/Game.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default class Game extends React.Component {
+    render() {
+        return <div>
+            <h1>Tic Tat Toe</h1>
+        </div>
+
+    }
+}
+
